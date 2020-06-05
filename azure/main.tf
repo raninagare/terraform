92d700afb3e6428aa30c30fd533095cad8e5a395
Prashant Gadagi <gadagip@vmware.com>
2020-06-05 16:04:01 -0700

    Updated Azure template

--- azure/main.tf
+++ azure/main.tf
@@ -1,23 +1,27 @@
 # Azure Subscription Id
-#variable "azure-subscription-id" {
-#    type        = string
-#    description = "Azure Subscription Id"
-#}
+variable "azure-subscription-id" {
+    type        = string
+    description = "Azure Subscription Id"
+    default = "test"
+}
+#Azure Client Id/appId
+variable "azure-client-id" {
+    type        = string
+    description = "Azure Client Id/appId"
+    default = "test"
+}
 # Azure Client Id/appId
-#variable "azure-client-id" {
-#    type        = string
-#    description = "Azure Client Id/appId"
-#}
-# Azure Client Id/appId
-#variable "azure-client-secret" {
-#    type        = string
-#    description = "Azure Client Id/appId"
-#}
+variable "azure-client-secret" {
+    type        = string
+    description = "Azure Client Id/appId"
+    default = "test"
+}
 # Azure Tenant Id
-#variable "azure-tenant-id" {
-#    type        = string
-#    description = "Azure Tenant Id"
-#}
+variable "azure-tenant-id" {
+    type        = string
+    description = "Azure Tenant Id"
+    default = "test"
+}
 
 provider "azurerm" { 
 #    subscription_id = var.azure-subscription-id
@@ -32,29 +36,29 @@ variable "prefix" {
   default = "vmware-gadagip-test"
 }
 
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-resources"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "vn" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
   address_prefix       = "10.0.2.0/24"
 }
 
-resource "azurerm_network_interface" "main" {
+resource "azurerm_network_interface" "ni" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -65,9 +69,9 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.ni.id]
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
