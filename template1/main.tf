7b64b67c7e81240cb0a29515ed37e6323c288ab5
Prashant Gadagi <gadagip@vmware.com>
2020-06-09 10:07:53 -0700

    Refactor

--- template1/main.tf
+++
@@ -1,8 +0,0 @@
-resource "random_string" "random" {
-    length = 16
-    special = true
-    override_special = "/@£$"
-    provisioner "local-exec" {
-        command = "sleep 2; echo slept"
-    }
-}
